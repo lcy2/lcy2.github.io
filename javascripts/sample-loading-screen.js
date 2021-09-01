58d250010ce22bb0ee335540624d7b066066bce3
Changyi Li <changyi@apple.com>
2021-08-31 23:00:17 -0700

    initial bend.html added

--- javascripts/sample-loading-screen.js
+++
@@ -1,7 +0,0 @@
-var slidUp = false;
-function slide_up(){
-  $('.dot1, .dot2').addClass('dots_disappear');
-  $('#loading').addClass('moved');
-  $('#loading_msg').addClass('dots_disappear');
-  $("body, html").css({"overflow":"initial"});
-}
